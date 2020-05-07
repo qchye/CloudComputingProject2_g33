bcf227dfef136b279ba5c3ceb9e47d1568ab855b
Sii Kim Lau <alvin_lau97@hotmail.com>
2020-05-07 12:44:19 +1000

    added more keywords

--- feature/TwitterHarvester.py
+++ feature/TwitterHarvester.py
@@ -3,42 +3,45 @@ import tweepy
 import json
 from re import search
 
-#Tweepy
-#Instead of using OAuthHandler, use AppAuthHandler (apprently can search more tweets and request more, dont know how but it is the case)
-#auth = tweepy.OAuthHandler("X7zGj3Ow4bPu05Em8WGGsko3G",
-                          # "VkYgRxRkid5Ru4cpU7QineZIK2icnpRIHpZpVGaH8RlSYCJIQG")
-                          #auth.set_access_token("1252170312889675777-tpP7OT9dstwFphXrF6PNyjWjJEGz22", 
-                   #  "2jJj69FgFr50w3MBt1yL6CUu97mgcffFac3rDpEuN5VYw")
+# Tweepy
+# Instead of using OAuthHandler, use AppAuthHandler (apprently can search more tweets and request more, dont know how but it is the case)
+# auth = tweepy.OAuthHandler("X7zGj3Ow4bPu05Em8WGGsko3G",
+# "VkYgRxRkid5Ru4cpU7QineZIK2icnpRIHpZpVGaH8RlSYCJIQG")
+# auth.set_access_token("1252170312889675777-tpP7OT9dstwFphXrF6PNyjWjJEGz22",
+#  "2jJj69FgFr50w3MBt1yL6CUu97mgcffFac3rDpEuN5VYw")
 
-#SEARCH API
-#Search result based on query given, giving tweets, check tweets condition
+# SEARCH API
+# Search result based on query given, giving tweets, check tweets condition
+
+
 def mainFunction(api, query, count, language, region):
 
     totalExplored = 0
     totalUsefulTweets = 0
     for keyword in query:
-        print("Key word currently being searched: %s"%keyword)
+        print("Key word currently being searched: %s" % keyword)
         for tweet in tweepy.Cursor(api.search, q=[keyword], count=count, lang=language).items():
 
             tweet_json = tweet._json
             totalExplored += 1
             print(tweet_json)
-            #We only interested the tweets in Australia and keyword in text
+            # We only interested the tweets in Australia and keyword in text
             if CheckTwitterLocation(tweet_json, region):
                 totalUsefulTweets += 1
-                #Tweets storing process here, waiting for CouchDb
+                # Tweets storing process here, waiting for CouchDb
 
                 print("==================================FINDING FRIENDS OF USER STARTS=============================================================================")
-                #Finding Friends of friends section
-                user=tweet_json['user']
+                # Finding Friends of friends section
+                user = tweet_json['user']
                 user_id = user['id']
                 friendsIdList = searchFriends(api, user_id)
-                totalExplored, totalUsefulTweets = ProcessRelatedTweets(api, friendsIdList, query, region, totalExplored, totalUsefulTweets)
-        
-            print("Total Explored: %d"%totalExplored)
-            print("Total Useful Tweets: %d"%totalUsefulTweets)
+                totalExplored, totalUsefulTweets = ProcessRelatedTweets(
+                    api, friendsIdList, query, region, totalExplored, totalUsefulTweets)
 
-    return 
+            print("Total Explored: %d" % totalExplored)
+            print("Total Useful Tweets: %d" % totalUsefulTweets)
+
+    return
 
 
 # Get a list of follower ids for the target account, input take in user id, output friend's ID as a list
@@ -48,27 +51,31 @@ def searchFriends(api, target):
 
     return friendsIDList
 
-#Finding friends' timeline tweets, also check if the tweets is useful, and record accordingly
-def ProcessRelatedTweets(api,friendsIdList, query, region, totalExplored, totalUsefulTweets):
-    
+# Finding friends' timeline tweets, also check if the tweets is useful, and record accordingly
+
+
+def ProcessRelatedTweets(api, friendsIdList, query, region, totalExplored, totalUsefulTweets):
+
     for Id in friendsIdList:
-        
+
         try:
-            #Searching tweets from timeline of user
+            # Searching tweets from timeline of user
             for tweet in tweepy.Cursor(api.user_timeline, id=Id).items():
                 totalExplored += 1
                 tweet_json = tweet._json
-                #print(tweet_json)
-                if CheckFriendsTwitter(tweet_json,query,region):
+                # print(tweet_json)
+                if CheckFriendsTwitter(tweet_json, query, region):
                     totalUsefulTweets += 1
-                print("Total Explored: %d"%totalExplored)
-                print("Total Useful Tweets: %d"%totalUsefulTweets)
-        except Exception: #Might because the user is private, so need to catch the exception.
+                print("Total Explored: %d" % totalExplored)
+                print("Total Useful Tweets: %d" % totalUsefulTweets)
+        except Exception:  # Might because the user is private, so need to catch the exception.
             pass
 
     return (totalExplored, totalUsefulTweets)
 
-#filter tweets: only the one match region
+# filter tweets: only the one match region
+
+
 def CheckTwitterLocation(tweet, region):
     useful = False
     text = tweet['text']
@@ -86,7 +93,9 @@ def CheckTwitterLocation(tweet, region):
 
     return useful
 
-#As going into friend's list, need to check all keywords
+# As going into friend's list, need to check all keywords
+
+
 def CheckFriendsTwitter(tweet, query, region):
     useful = False
     text = tweet['text']
@@ -101,27 +110,35 @@ def CheckFriendsTwitter(tweet, query, region):
     if (loc in region):
         for w in query:
             if w.lower() in text.lower():
-                print("found xD")
+                print("found & Current Keyword = %s" % w.lower())
                 useful = True
 
     return useful
 
+
 def main():
 
-    auth = tweepy.AppAuthHandler("X7zGj3Ow4bPu05Em8WGGsko3G", "VkYgRxRkid5Ru4cpU7QineZIK2icnpRIHpZpVGaH8RlSYCJIQG")
-    api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
-    region=['Melbourne','Sydney','Quensland','Australia','Western Australia','South Australia','Victoria']
-    keywords=['uber', 'airtasker', 'freelancer', 'deliveroo']
-    region=['AU','Melbourne','Sydney','Queensland','Australia','Western Australia','South Australia','Victoria']
+    auth = tweepy.AppAuthHandler(
+        "PmRzO6aE5UCwbU9e9hUWS0PKv", "uV88V1zKlqyEIayVfWnLfCXyTKJngT2u6RUaGO4SpIEfeM4QLm")
+    api = tweepy.API(auth, wait_on_rate_limit=True,
+                     wait_on_rate_limit_notify=True)
+    keywords = ['airbnb', 'stayz', 'zomato', 'deliveroo', 'hungrypanda',
+                'lyft', 'olacab', '#grab', 'grabcar', 'didirider', 'menulog', 'taxify', 'etsy', 'gumtree', 'fiverr', 'cookitoo', 'uber', 'expedia', 'airtasker', 'freelancer', 'parkhound', 'campspace',
+                'upwork', 'designcrowd', 'ratesetter', 'urbansitter', 'airly', 'gocatch', 'shebah', 'bellhops', 'channel40', 'freightmatch', 'wrappli', 'zoom2u', 'carnextdoor', 'camplify', 'kindershare', 'quipmo', 'thevolte', 'bettercaring', '#blys', 'classbento', 'helpling']
+    region = ['AU', 'Melbourne', 'Sydney', 'Queensland', 'Australia',
+              'Western Australia', 'South Australia', 'Victoria']
     count = 10000
     language = 'en'
     mainFunction(api, keywords, count, language, region)
 
     return
 
+
 main()
 
-#Stream API goes here
+# Stream API goes here
+
+
 class MyStreamListener(tweepy.StreamListener):
 
     def __init__(self, time_limit=60):
@@ -134,8 +151,8 @@ class MyStreamListener(tweepy.StreamListener):
     def on_data(self, data):
         print(data)
 
+
 def StartStream(keywordList, language):
     myStreamListener = MyStreamListener()
-    myStream = tweepy.Stream(auth = auth1, listener=myStreamListener)
+    myStream = tweepy.Stream(auth=auth1, listener=myStreamListener)
     myStream.filter(track=keywordList, languages=language)
-
