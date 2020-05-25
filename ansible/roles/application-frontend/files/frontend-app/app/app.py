ae95fc4cadb176798a3f3e4abca132374d3bd0ab
Unknown <rohan.australia@gmail.com>
2020-05-25 15:43:21 +1000

    added some html

--- ansible/roles/application-frontend/files/frontend-app/app/app.py
+++ ansible/roles/application-frontend/files/frontend-app/app/app.py
@@ -23,7 +23,21 @@ def stateCount():
     filepath = controller.GetLocalGig()
 
     return render_template('home.html', path=filepath, searched= True)
-
+# Scenario
+@app.route('/scenario', methods=['GET', 'POST'])
+def scenario():
+    if request.method == 'POST':
+        id = request.form['id']
+        if id == "yun":
+            return render_template('about.html')
+        if id == "Mean Income vs Gig Economy Related Tweets":
+            return render_template('income_tweet.html')
+        if id == "Gig Economy Business Popularity":
+            return render_template('tweet_pop.html')
+        if id == "Unemployment vs Gig Economy Related Tweets":
+            return render_template('unemp_tweet.html')
+        if id == "alvin":
+            return render_template('about.html')
 
 if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0')
